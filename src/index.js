cd4566b9efd28286a432314860049e17d0fca0b0
oron01 <136211946+oron01@users.noreply.github.com>
2023-12-03 02:17:38 +0200

    complete the menu

--- src/index.js
+++ src/index.js
@@ -84,14 +84,27 @@ let createMenus = () => {
         menusContent.innerHTML = ""
         switch (selectedMenu) {
             case "Entrees":
-                createItem("Cilantro Crawfish Gumbo","The Arugula Caesar Salad rebels against convention with its peppery arugula leaves, challenging the traditional romaine. Tossed in a clandestine Caesar dressing with anchovy nuances and adorned with defiant croutons, it's a culinary insurrection on the palate.")
+                createItem("Cilantro Crawfish Gumbo","A tantalizing Cajun serenade, melds the bold zest of cilantro with the sultry dance of crawfish, creating a symphony of flavors that echoes through the bayou.")
                 createItem("Mud Soup","A daring divergence, Mud Soup captivates with avant-garde earthiness, defying culinary norms in a bold journey through unconventional cuisine's untamed wilderness.")
                 createItem("Red snapper with violets and pine nuts","The Red Snapper, a piscatorial virtuoso, elegantly bedecked with violets and pine nuts, orchestrates a sensory concerto on the palate.")
                 createItem("Charcola Arugula","A darkened rebellion against the usual verdant fare, Charcoal Arugula emerges as a mysterious and shadowy kaleidoscope on the plate, infusing intrigue into every bite.")
                 break;
-            default:
+            case "Cuisine":
+                createItem("Sea Urchin Ceviche","A great initiation into indulgence, the Sea Urchin Ceviche unfolds as a masterpiece of marine delight.")
+                createItem("Peanut Butter Soup with Smoked Duck and Mashed Squash", " a flavorful fusion where rich peanut butter meets smoky duck and sweet mashed squash, creating a harmonious culinary masterpiece.")
+                createItem("Squid Ravioli in a Lemongrass Broth with Goat Cheese Profiteroles","An exquisite voyage through the seas and gardens of gastronomy, the Squid Ravioli in a Lemongrass Broth with Goat Cheese Profiteroles is a culinary sonnet.")
+                createItem("Pork Loin with Lime Jell-o","A daring culinary juxtaposition, the Pork Loin with Lime Jell-o redefines flavor expectations.")
+                createItem("Swordfish Meatloaf with Onion Marmalade", "A bold reinterpretation of a classic, takes meatloaf to the heights of oceanic elegance.")
                 break;
-        }    }
+            case "Spirits":
+                createItem("Vodka Mirage", "Vodka + Cranberry Juice + Splash of Club Soda")
+                createItem("Gin Sonata","Gin + Dry Vermouth + Lemon Twist")
+                createItem("Mystic Mule", "Bourbon + Ginger Beer + Fresh Lime")
+            case "Delicacies":
+                createItem("Caramelized Pecan Parfait","Vanilla Bean Ice Cream combined with Warm Caramel Sauce and Toasted Pecans")
+                createItem("Dark Chocolate Decadence","Flourless Chocolate Cake with Raspberry Coulis and a Dollop of Whipped Cream")
+                createItem("Lemon Infusion Tart","Lemon Curd on Buttery Shortcrust, topped with Zest of Fresh Lemons")
+            }    }
     buttons.forEach((button) => {menusDiv.appendChild(button)
     button.classList = "menusButton"
     button.addEventListener("click",menusButtonsHandler)})
